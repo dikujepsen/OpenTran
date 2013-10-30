dbe572cdc41b47c7b789102ae9c56bffcea6da19
dikujepsen <jepsen@diku.dk>
2013-10-30 22:55:03 +0100

    for loop header

--- test/matmulfunc.cpp
+++ test/matmulfunc.cpp
@@ -1,15 +1,47 @@
-#define START 0.0
+int t = 5;
+float g = 5.0;
+char * str = "32423";
+t = 5;
+g = 5.0;
+str = "32423";
+void jod;
+jod = g;
+//#define START 0.0 asdasd
 
-void
-matmul(float* A, float* B, float* C, unsigned hA, unsigned wA, unsigned wB)
-{
-  char * str = "asda2d1dasdw22d";
-    for (unsigned i = 0; i < hA; ++i)
-        for (unsigned j = 0; j < wB; ++j) {
-            float sum = START;
-            for (unsigned k = 0; k < wA; ++k) {
-                sum += A[i * wA + k] * B[k * wB + j];
-            }
-            C[i * wB + j] = sum;
-        }
+void jod2;
+int longid;
+float * A;
+void matmul();
+void matmul(float* A);
+void matmul(float* A, unsigned B);
+void matmul(float* A, float* B, float* C, unsigned hA, unsigned wA, unsigned wB);
+{int longid;
+float * A;
 }
+void matmul()
+{int longid;
+float * A;
+}
+
+(unsigned i = 0; i < hA; i++)
+
+t = -3;
+t = i < hA;
+void matmul(float* A, float* B, float* C, unsigned hA, unsigned wA, unsigned wB)
+{
+  char * str;
+  char * str = "asda2d1dasdw22d";
+}
+
+// matmul(float* A, float* B, float* C, unsigned hA, unsigned wA, unsigned wB)
+// {
+//char * str = "asda2d1dasdw22d";
+//     for (unsigned i = 0; i < hA; i++) 
+//         for (unsigned j = 0; j < wB; j++) {
+//             float sum = START;
+//             for (unsigned k = 0; k < wA; k++) {
+//                 sum += A[i * wA + k] * B[k * wB + j];
+//             }
+//             C[i * wB + j] = sum;
+//         }
+// }
