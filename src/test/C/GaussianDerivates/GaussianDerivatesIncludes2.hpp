29a5dcfc0a34ec7dac5f5ac6e252397838b740f4
dikujepsen <jepsen@diku.dk>
2014-02-28 10:44:42 +0100

    Added GFlops calculations

--- src/test/C/GaussianDerivates/GaussianDerivatesIncludes2.hpp
+++ src/test/C/GaussianDerivates/GaussianDerivatesIncludes2.hpp
@@ -13,13 +13,13 @@ float He(const int n,
     v = x*x-1;
     break;
   case 3:
-    v = native_powr(x,3)-3*x;
+    v = native_pown(x,3)-3*x;
     break;
   case 4:
-    v = native_powr(x,4)-6*x*x+3;
+    v = native_pown(x,4)-6*x*x+3;
     break;
   case 5:
-    v = native_powr(x,5)-10*native_powr(x,3)+15*x;
+    v = native_pown(x,5)-10*native_pown(x,3)+15*x;
     break;
   default:
     ;
@@ -34,12 +34,11 @@ float DaKs(int da[3], float xmy[3], float r, float ks) {
   float sum_da = 0.0;
   for (int k = 0; k < 3; k++) {
     z[k] = native_sqrt(2.0f)/r*xmy[k];
-
     sum_da += da[k];
   }
   
   float res;
-  res = native_powr(-native_sqrt(2.0f)/r, sum_da)*He(da[0],z[0])*He(da[1],z[1])*He(da[2],z[2])*ks;
+  res = native_pown(-native_sqrt(2.0f)/r, sum_da)*He(da[0],z[0])*He(da[1],z[1])*He(da[2],z[2])*ks;
   return res;
 }
 
--- src/test/PGI_OpenACC/common.mk
+++ src/test/PGI_OpenACC/common.mk
@@ -4,7 +4,7 @@ EXECDEP = $(EXEC:.exe=.cpp)
 
 LNK_LIB =  -fast -Minline=levels:3 -acc -Minfo=accel 
 
-INC_LIB = /usr/include/x86_64-linux-gnu -Msafeptr
+INC_LIB = /usr/include/x86_64-linux-gnu -Msafeptr -ta=kepler
 
 #CC = /home/xhz206/PGI_Compiler/linux86-64/13.10/bin/pgCC
 #CC = /home/jacob/PGI_Compiler/linux86-64/13.10/bin/pgCC
